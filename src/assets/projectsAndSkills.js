296ab4ecb701c655293f2d9b6d1fbd484cff0ff6
Douglas Lovera <doulovera@gmail.com>
2021-05-30 20:54:58 -0500

    Redesign project cards buttons. Comment Resume button (until I do it hehe)

--- src/assets/projectsAndSkills.js
+++ src/assets/projectsAndSkills.js
@@ -13,79 +13,85 @@ discordjs
 */
 
 const ProjectList = [
-    {
-        name: 'OneOne - Web',
-        preview: 'https://res.cloudinary.com/dyonw3lkf/image/upload/v1621379375/Portfolio%20images/oneone_preview_fvrqdd_1_aq8mcw.png',
-        languages: {
-            html: true,
-            css: true,
-            js: true,
-            discordjs: true
-        },
-        url: 'https://oneonebot.ml/'
+  {
+    name: "OneOne - Web",
+    preview:
+      "https://res.cloudinary.com/dyonw3lkf/image/upload/v1621379375/Portfolio%20images/oneone_preview_fvrqdd_1_aq8mcw.png",
+    languages: {
+      html: true,
+      css: true,
+      js: true,
+      discordjs: true,
     },
-    {
-        name: 'Github Profile Finder',
-        preview: 'https://res.cloudinary.com/dyonw3lkf/image/upload/v1621379376/Portfolio%20images/ghprofile_ovz5x7_1_wuinkh.png',
-        languages: {
-            html: true,
-            css: true,
-            js: true,
-        },
-        url: 'https://gh-profile-finder.netlify.app/'
+    demoUrl: "https://oneonebot.ml/",
+    // repoUrl: "https://github.com/doulovera/oneone-web",
+  },
+  {
+    name: "Github Profile Finder",
+    preview:
+      "https://res.cloudinary.com/dyonw3lkf/image/upload/v1621379376/Portfolio%20images/ghprofile_ovz5x7_1_wuinkh.png",
+    languages: {
+      html: true,
+      css: true,
+      js: true,
     },
-    {
-        name: 'Publifelix',
-        preview: 'https://res.cloudinary.com/dyonw3lkf/image/upload/v1621379376/Portfolio%20images/publifelix_dw5fbm_1_mafdyp.png',
-        languages: {
-            html: true,
-            css: true,
-            js: true,
-            bootstrap: true,
-        },
-        url: 'https://publifelix-demo.netlify.app/'
+    demoUrl: "https://gh-profile-finder.netlify.app/",
+    repoUrl: "https://github.com/doulovera/github-profile-finder",
+  },
+  {
+    name: "Publifelix",
+    preview:
+      "https://res.cloudinary.com/dyonw3lkf/image/upload/v1621379376/Portfolio%20images/publifelix_dw5fbm_1_mafdyp.png",
+    languages: {
+      html: true,
+      css: true,
+      js: true,
+      bootstrap: true,
     },
+    demoUrl: "https://publifelix-demo.netlify.app/",
+    repoUrl: "https://github.com/doulovera/publifelix-demo",
+  },
 ];
 
 const SkillsList = [
-    {
-        name: 'HTML5',
-        background: '#f06529'
-    },
-    {
-        name: 'CSS3',
-        background: '#2965f1'
-    },
-    {
-        name: 'Responsive',
-        background: '#FF69B4'
-    },
-    {
-        name: 'JavaScript',
-        background: '#F0DB4F',
-        color: '#323330'
-    },
-    {
-        name: 'TypeScript',
-        background: '#007acc'
-    },
-    {
-        name: 'React.js',
-        background: '#61DBFB',
-        color: '#1F232A'
-    },
-    {
-        name: 'Git/Github',
-        background: '#f34f29'
-    },
-    {
-        name: 'Node.js (Basic)',
-        background: '#3C873A'
-    },
-    {
-        name: 'Figma (Basic)',
-        background: '#8B37FF'
-    },
-]
+  {
+    name: "HTML5",
+    background: "#f06529",
+  },
+  {
+    name: "CSS3",
+    background: "#2965f1",
+  },
+  {
+    name: "Responsive",
+    background: "#FF69B4",
+  },
+  {
+    name: "JavaScript",
+    background: "#F0DB4F",
+    color: "#323330",
+  },
+  {
+    name: "TypeScript",
+    background: "#007acc",
+  },
+  {
+    name: "React.js",
+    background: "#61DBFB",
+    color: "#1F232A",
+  },
+  {
+    name: "Git/Github",
+    background: "#f34f29",
+  },
+  {
+    name: "Node.js (Basic)",
+    background: "#3C873A",
+  },
+  {
+    name: "Figma (Basic)",
+    background: "#8B37FF",
+  },
+];
 
-export {ProjectList, SkillsList};
+export { ProjectList, SkillsList };
--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,24 +1,60 @@
-import React from 'react'
-import './style.css'
+import React from "react";
+import "./style.css";
 
 export default function Footer() {
-    return (
-        <footer>
-            <section className="footer__links">
-                <ul>
-                    <li><a href="https://dev.to/doulovera" target="_blank" rel="noreferrer">Blog</a></li>
-                    <li><a href="https://discord.gg/Pc7CtmgUk2" target="_blank" rel="noreferrer">Discord</a></li>
-                    <li><a href="https://github.com/doulovera" target="_blank" rel="noreferrer">Github</a></li>
-                    <li><a href="https://twitter.com/doulovera" target="_blank" rel="noreferrer">Twitter</a></li>
-                    <li><a href="https://github.com/doulovera/doulovera.github.io" target="_blank" rel="noreferrer">Source</a></li>
-                </ul>
-            </section>
-            <section className="footer__resume">
-                <a href="#3">My Resume <i className="fas fa-download"></i></a>
-            </section>
-            <section className="footer__copyright">
-                <p>&copy; Made by Douglas Lovera - 2021</p>
-            </section>
-        </footer>
-    )
+  return (
+    <footer>
+      <section className="footer__links">
+        <ul>
+          <li>
+            <a href="https://dev.to/doulovera" target="_blank" rel="noreferrer">
+              Blog
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://discord.gg/Pc7CtmgUk2"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Discord
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://github.com/doulovera"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Github
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://twitter.com/doulovera"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Twitter
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://github.com/doulovera/doulovera.github.io"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Source
+            </a>
+          </li>
+        </ul>
+      </section>
+      <section className="footer__resume">
+        {/* <a href="#3">My Resume <i className="fas fa-download"></i></a> */}
+      </section>
+      <section className="footer__copyright">
+        <p>&copy; Made by Douglas Lovera - 2021</p>
+      </section>
+    </footer>
+  );
 }
