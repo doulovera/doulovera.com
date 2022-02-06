48cb1d85ca880e75733dc1a99e6a6537b32989d8
Douglas Lovera <doulovera@gmail.com>
2022-02-05 22:44:30 -0500

    feat: header done (missing open/close indicator)

--- main.js
+++ main.js
@@ -5,15 +5,17 @@ const app = document.querySelector('#app');
 
 // toggle dark/light mode
 const toggleThemeBtn = document.getElementById('toggle-theme');
+toggleClass(toggleThemeBtn, document.body, 'light-theme');
+
 // open navbar on mobile
 const openHeaderBtn = document.querySelector('.header-open_nav');
 const header = document.querySelector('.header-container');
+toggleClass(openHeaderBtn, header, 'header-open');
 
-const toggleClass = (element, toggledElement, className) => {
+
+
+function toggleClass (element, toggledElement, className) {
   return element.addEventListener('click', () => {
     toggledElement.classList.toggle(className);
   });
 };
-
-toggleClass(toggleThemeBtn, document.body, 'light-theme');
-toggleClass(openHeaderBtn, header, 'header-open');
