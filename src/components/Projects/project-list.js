4a631a5c0ac1fbc85dade50b599e79a3a77dba75
Douglas Lovera <doulovera@gmail.com>
2022-09-08 16:14:48 -0500

    feat: add inter as font and improve cards design

--- src/components/Projects/project-list.js
+++ src/components/Projects/project-list.js
@@ -1,45 +1,65 @@
+const languages = {
+  javascript: 'JavaScript',
+  typescript: 'TypeScript',
+  node: 'Node.js',
+};
+
+const frameworks = {
+  react: 'React.js',
+  next: 'Next.js',
+};
+
+const css = {
+  tailwind: 'Tailwind',
+  bootstrap: 'Bootstrap',
+};
+
+const services = {
+  twilio: 'Twilio',
+};
+
 const ProjectList = [
   {
     image: 'https://res.cloudinary.com/dyonw3lkf/image/upload/v1659116121/portfolio_images/msedge_7XfaNqGY5x_d42o0a.png',
     title: 'Chat-App',
     repository: 'https://github.com/doulovera/chat-app',
     demo: 'https://chat-app-2022.vercel.app/',
-    technologies: ['TypeScript', 'React.js', 'Next.js', 'Tailwind', 'Twilio'],
+    technologies: [languages.typescript, frameworks.react, frameworks.next, css.tailwind, services.twilio],
   },
   {
     image: 'https://res.cloudinary.com/dyonw3lkf/image/upload/v1645307597/portfolio_images/im-a-gen_compress_oyv10s.jpg',
     title: 'Im-a-gen',
     repository: 'https://github.com/doulovera/im-a-gen',
     demo: 'https://im-a-gen.netlify.app/',
-    technologies: ['TypeScript', 'React.js'],
+    technologies: [languages.typescript, frameworks.react],
   },
   {
     image: 'https://res.cloudinary.com/dyonw3lkf/image/upload/v1645307597/portfolio_images/numrify_compress_kmtac9.jpg',
     title: 'Numrify',
     repository: 'https://github.com/doulovera/im-a-gen',
     demo: 'https://numrify.netlify.app/',
-    technologies: ['JavaScript', 'React.js'],
+    technologies: [languages.javascript, frameworks.react],
   },
   {
     image: 'https://res.cloudinary.com/dyonw3lkf/image/upload/v1645307597/portfolio_images/oneone-web_compress_i4z5e9.jpg',
     title: 'One-One Discord Bot',
     repository: 'https://github.com/doulovera/oneone-web',
     demo: 'https://oneone.netlify.app/',
-    technologies: ['JavaScript'],
+    technologies: [languages.javascript],
   },
   {
     image: 'https://res.cloudinary.com/dyonw3lkf/image/upload/v1645307597/portfolio_images/publifelix_compress_obhrik.jpg',
     title: 'Publifelix',
     repository: 'https://github.com/doulovera/publifelix-demo',
     demo: 'https://publifelix-demo.netlify.app/',
-    technologies: ['Bootstrap', 'JavaScript'],
+    technologies: [css.bootstrap, languages.javascript],
   },
   {
     image: 'https://res.cloudinary.com/dyonw3lkf/image/upload/v1645307597/portfolio_images/todo-app_compress_mbyxiu.jpg',
     title: 'ToDo App',
     repository: 'https://github.com/doulovera/todo-app_react',
     demo: 'https://dl-note-taker.vercel.app/',
-    technologies: ['JavaScript', 'React.js'],
+    technologies: [languages.javascript, frameworks.react],
   },
 ]
 
