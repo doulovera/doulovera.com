7a41c97cc9abb499c13adf7c52c2dbf76f23a49f
Douglas Lovera <doulovera@gmail.com>
2023-04-02 12:48:20 -0500

    use phosphor svg icons instead of the library

--- src/components/SocialMedia/social-links.js
+++ src/components/SocialMedia/social-links.js
@@ -1,23 +1,18 @@
 export const RESUME = {
   name: "Resume",
   url: "https://drive.google.com/file/d/1KPvoF7mInAczBWZO1YrNPgSVcq62C2hV/view",
-  icon: "ph-download-simple",
+  icon: "ph:download-simple",
 };
 
 export const SOCIAL_LINKS = [
   {
     name: "LinkedIn",
     url: "https://linkedin.com/in/douglaslovera/",
-    icon: "ph-linkedin-logo",
+    icon: "ph:linkedin-logo",
   },
   {
     name: "GitHub",
     url: "https://github.com/doulovera/",
-    icon: "ph-github-logo",
-  },
-  {
-    name: "Twitter",
-    url: "https://twitter.com/doulovera/",
-    icon: "ph-twitter-logo",
+    icon: "ph:github-logo",
   },
 ];
