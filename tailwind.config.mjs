4108e716d4b1e571fa4396e7f8e13cdd34d4f363
Douglas Lovera <doulovera@gmail.com>
2024-05-12 14:16:00 -0500

    use box shadow as outline and fix width for social network in firefox

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,11 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			boxShadow: {
+				'outline': '0 0 0 2px black'
+			}
+		},
 	},
 	plugins: [],
 }
