cd07d5c2bc08ec7f373ad6f1dd184b006a909365
Douglas Lovera <doulovera@gmail.com>
2024-05-14 19:14:57 -0500

    add experience section

---
+++ src/constants/experience.ts
@@ -0,0 +1,16 @@
+export const experience = [
+  {
+    from: new Date("2021-11-01"),
+    to: new Date(),
+    isCurrent: true,
+    company: "Rhinolabs",
+    role: "Fullstack Developer",
+    image: 'rhinolabs.jpg',
+    description: `Working with the team of MyAccident.org to add new features and improve the current ones. Adding unit test to backend endpoints and frontend components with Jest and react-testing-library.`,
+    items: [
+      "Developed new features for the MyAccident.org platform",
+      "Improved the current features",
+      "Added unit tests to the backend and frontend codebase"
+    ],
+  },
+];
