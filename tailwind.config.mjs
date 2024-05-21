08d5eb62b5a23c8fb353e015afae0d28f16fcd66
Douglas Lovera <doulovera@gmail.com>
2024-05-20 23:28:06 -0500

    add more projects section and clean up components

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -7,6 +7,8 @@ export default {
 				'outline': '0 0 0 2px black',
 				'outline_gray': '0 0 0 2px #e5e7eb',
 				'outline-no-left': '1px 1px 0 1px black, 1px -1px 0 1px black',
+				'outline-no-bottom': '-1px -1px 0 1px black, 1px -1px 0 1px black',
+				'outline_gray-no-bottom': '-1px -1px 0 1px #e5e7eb, 1px -1px 0 1px #e5e7eb',
 			}
 		},
 	},
