a4801cd05802c7696f8f12120e127cde791b1174
Douglas Lovera <doulovera@gmail.com>
2024-06-08 22:05:57 -0500

    add support for dark-mode

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,19 +1,31 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+	darkMode: [
+		'variant', [
+			'@media (prefers-color-scheme: dark) { &:not([scheme="light"] *) }',
+			'&:is([scheme="dark"] *)',
+		]
+	],
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {
 			boxShadow: {
-				'outline': '0 0 0 2px black',
+				'outline': '0 0 0 2px',
 				'outline_gray': '0 0 0 2px #e5e7eb',
-				'outline-no-left': '1px 1px 0 1px black, 1px -1px 0 1px black',
-				'outline-no-bottom': '-1px -1px 0 1px black, 1px -1px 0 1px black',
-				'outline_gray-no-bottom': '-1px -1px 0 1px #e5e7eb, 1px -1px 0 1px #e5e7eb',
+				'outline-no-left': '1px 1px 0 1px, 1px -1px 0 1px',
+				'outline-no-bottom': '-1px -1px 0 1px, 1px -1px 0 1px',
 			},
 			colors: {
 				brand: {
-					DEFAULT: '#dbeafe'
-				}
+					DEFAULT: '#dbeafe',
+					dark: '#4d5b92',
+				},
+				light: {
+					DEFAULT: '#fff'
+				},
+				dark: {
+					DEFAULT: '#000'
+				},
 			}
 		},
 	},
