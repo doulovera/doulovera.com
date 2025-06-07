5811bf5fa9d361b83c3ee3c1c3a3eb5ef6c748b6
Douglas Lovera <doulovera@gmail.com>
2025-06-06 20:09:14 -0500

    feat: add tailwind vite package and implement to astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,16 @@
 import { defineConfig } from "astro/config";
 
-import tailwind from "@astrojs/tailwind";
+import tailwindcss from "@tailwindcss/vite";
 
-// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  vite: {
+    plugins: [tailwindcss()],
+  },
   i18n: {
     defaultLocale: 'en',
     locales: ['en', 'es'],
     routing: {
       prefixDefaultLocale: false,
     }
-  }
+  },
 });
